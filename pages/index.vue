f7e5d12bfd29e7e865d8507a5cbeaa62fceaeff0
MPrint USA Dom <dominickmvillano@gmail.com>
2021-07-14 16:17:34 -0400

    Added form button and changed links

--- pages/index.vue
+++ pages/index.vue
@@ -10,26 +10,62 @@
       </h2>
       <div class="links">
         <a
-          href="url"
+          href="www.mprintusa.com"
           target="_blank"
           rel="noopener"
           class="button--green"
         >
-          Contact
+          Printing
         </a>
         <a
-          href="contact.vue"
+          href="mailto:orders@thelifesavingstation.com"
           target="_blank"
           rel="noopener"
           class="button--grey"
         >
-          About
+          Contact Us!
         </a>
       </div>
     </div>
   </div>
 </template>
 
+<!-- Read the Formbutton docs at formspree.io/formbutton/docs. See more examples at codepen.io/formspree -->
+<script src="https://formspree.io/js/formbutton-v1.min.js" defer></script>
+<script>
+  /* paste this line in verbatim */
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+
+  /* customize formbutton below*/
+  formbutton("create", {
+    action: "https://formspree.io/f/xknkvboz",
+    title: "How can we help?",
+    fields: [
+      {
+        type: "email",
+        label: "Email:",
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }
+    ],
+    styles: {
+      title: {
+        backgroundColor: "gray"
+      },
+      button: {
+        backgroundColor: "gray"
+      }
+    }
+  });
+</script>
 
 
 <script>
